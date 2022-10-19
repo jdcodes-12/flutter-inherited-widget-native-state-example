39b9417daeb332fa4bb1b217f00a9a11dcb2ef24
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-10-18 21:59:43 -0500

    Add UUID package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
+/*
+ * InheritedWidget is like a hybrid of Statless & StatefulWidget.
+ * It allows you to hold on to state that it's children widgets can use.
+ * It has final fields & const constructor. It's primary goal is to 
+ * carry state throughout the WidgetTree to pass state between each
+ * Widget in the component if needed. InheritedWidget gives you control
+ * over which Widget you choose to redraw, unlike normal redraws, because
+ * it allows you to see if the changes in a widget should be redrawn 
+ * or not.
+ * 
+ * Note that InheritedWidget itself is not a StatefulWidget. It's a constant.
+ * It comes itself to the old InheritedWidget, seeing if the previous
+ * InheritedWidget has changed enough to be redrawn. This best way to 
+ * compare InheritedWidgets is to give them a unique id. To do this, 
+ * one can use uuid package.
+ */
+
 void main() {
   runApp(
     MaterialApp(
