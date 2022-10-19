fb4b5c47b0213be6e1cb914d22afd944c30cde62
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-10-18 21:38:42 -0500

    Add simple state and GestureDector to HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,37 @@ void main() {
   );
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+/*
+ * Creating variables in the `_HomePageState` class is
+ * similar to setting state using `setState()` in react.
+ * 
+ */
+class _HomePageState extends State<HomePage> {
+  String title = 'Tap the screen';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page - InheritedWidget'),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          // SetState comes from framework.dart
+          setState(() {
+            title = DateTime.now().toIso8601String();
+          });
+        },
+        child: Container(
+          color: Colors.white,
+        ),
       ),
     );
   }
